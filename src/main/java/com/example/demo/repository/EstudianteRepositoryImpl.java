dc5a3a36c3cca1571c8b3c3e05a1bcdd9b62055f
wendyjazmin <wendyjazmin1994@hotmail.com>
2024-01-27 02:11:10 -0500

    cambiando de carpeta

--- src/main/java/com/example/demo/repository/EstudianteRepositoryImpl.java
+++ src/main/java/com/example/demo/repository/EstudianteRepositoryImpl.java
@@ -1,8 +1,8 @@
-package com.example.demo.repository.modelo;
+package com.example.demo.repository;
 
 import org.springframework.stereotype.Repository;
 
-import com.example.demo.repository.IEstudianteRepository;
+import com.example.demo.repository.modelo.Estudiante;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
